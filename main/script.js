5a89a8ac6b341710028c34e9f112ab997ef84503
moeebii <ebiim621@newschool.edu>
2024-05-11 15:17:48 -0400

    e

--- main/script.js
+++ main/script.js
@@ -7,13 +7,32 @@ navigator.mediaDevices.getUserMedia({ video: true })
         console.error('Error accessing the webcam: ', err);
     });
 var images = [
-    "scan1.jpeg",
-    "scan2.jpeg",
-    "scan3.jpeg",
-    "scan4.jpeg",
-    "scan5.jpeg",
-    "scan6.jpeg",
-    "scan7.jpeg"
+    "scan1.png",
+    "scan2.png",
+    "scan3.png",
+    "scan4.png",
+    "scan5.png",
+    "scan6.png",
+    "scan7.png",
+    "scan8.png",
+    "scan9.png",
+    "scan10.png",
+    "scan11.png",
+    "scan12.png",
+    "scan13.png",
+    "scan14.png",
+    "scan15.png",
+    "scan16.png",
+    "scan17.png",
+    "scan18.png",
+    "scan19.png",
+    "scan20.png",
+    "scan21.png",
+    "scan22.png",
+    "scan23.png",
+    "scan24.png",
+    "scan25.png",
+    "scan26.png"
 ];
 
 function getRandomImage() {
